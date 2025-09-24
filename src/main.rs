47c73767859055fc45da486b12c56ee6fd577c02
shenef <matusik.rene@gmx.de>
2025-09-24 08:53:45 +0200

    Squashed commit of the following:
    commit 9c9dbff39ba2b1f0c0607b324bde7139c57f4858
    Author: shenef <matusik.rene@gmx.de>
    Date:   Wed Sep 24 08:50:26 2025 +0200
    
        fix rebase
    
    commit bb9c60c29d5b20236e451a51a0bf0edadccb2146
    Author: shenef <matusik.rene@gmx.de>
    Date:   Tue Sep 23 12:07:15 2025 +0200
    
        clean up extra spaces
    
    commit ce9063a4c5dc5ef986e19477f68327fb7caf493d
    Author: shenef <matusik.rene@gmx.de>
    Date:   Tue Sep 23 11:57:19 2025 +0200
    
        embed video
    
    commit dddd4668e24b166d3263fd47aeb4ec6d72ac7872
    Author: shenef <matusik.rene@gmx.de>
    Date:   Tue Sep 23 11:36:41 2025 +0200
    
        reformat and extend readme and help output
    
    commit 5d249720886d91465d6d406b07de1b6849ad2f56
    Author: shenef <matusik.rene@gmx.de>
    Date:   Tue Sep 23 11:25:34 2025 +0200
    
        create gitignore
    
    commit 487f53e5489f1431ff265f72cf3548a4fca99ca1
    Author: emrakyz <emreakyuz_2@hotmail.com>
    Date:   Tue Sep 23 01:42:16 2025 +0200
    
        Remove TQ based stuff for now
    
    commit dbf3fbecfba895aaaa92ba122f7804bff7522aa2
    Author: emrakyz <emreakyuz_2@hotmail.com>
    Date:   Mon Sep 22 22:55:49 2025 +0200
    
        Initial commit - XAV v0.1.0

--- src/main.rs
+++ src/main.rs
@@ -36,30 +36,33 @@ extern "C" fn exit_restore(_: i32) {
 
 #[rustfmt::skip]
 fn print_help() {
+    // Update readme when modifying this!
+    println!("Format: xav [options] <INPUT> [<OUTPUT>]");
+    println!();
+    println!("<INPUT>        Input file path");
+    println!("<OUTPUT>       Output file path. Appends `_av1.mkv` to the input name if not specified");
+    println!();
+    println!("Options:");
+    println!("--help         Show this help message");
+    println!("-w|--worker    Number of encoder instances to run. If not specified, selects generic values for `--worker` and `--lp`");
+    println!("-s|--sc        Scene change file to use. Runs av-scenechange and creates a SCD file if not specified");
+    println!("-r|--resume    Add it to same cmd or use with the input file");
+    println!("-h|--high-mem  Disables bit-packing and 10 bit conversion is handled by the decoder thread");
+    println!("-q|--quiet     Do not run any codepaths related to progress display");
+    println!();
+    println!("TQ:");
+    println!("WORK IN PROGRESS");
+    // println!("-t|--tq        Allowed SSIMU2 Range for Target Quality. Takes single values or ranges formatted like `70.0-75.0`");
+    // println!("-m|--mode      TQ metric evaluation mode. `mean` or percentile mean formatted like `p15`");
+    // println!("-c|--qp        Allowed CRF/QP search range for Target Quality. Takes single values or ranges formatted like `10.0-55.0`");
+    println!();
     println!("Examples:");
     println!("xav -r i.mkv");
     println!("xav -w 8 -s sc.txt -p \"--lp 3 --tune 0\" i.mkv o.mkv");
     println!(
-        "xav -q -l -w 8 -s sc.txt -t 70-75 -c 4-70 -m mean -p \"--lp 3 --tune 0\" i.mkv o.mkv"
+        "xav -q -h -w 8 -s sc.txt -t 70-75 -c 4-70 -m mean -p \"--lp 3 --tune 0\" i.mkv o.mkv"
     );
-    println!("xav i.mkv  # Use encoder defaults, add `_av1` to the input name.");
-    println!();
-    println!("Plain:");
-    println!("-w|--worker 8        No of encoders to run");
-    println!("-s|--sc scd.txt      SCD file to use");
-    println!("-r|--resume          Add it to same cmd or use with the input file");
-    println!("-h|--high-mem        Enable high memory mode: ");
-    println!("                     Default Low Mem: Bit-pack 10b or convert to 10b on worker threads");
-    println!("                     High mem: Keep full data, do everything on decoder thread");
-    println!("-q|--quiet           Do not run any progress related codepaths");
-    println!("<INPUT>              In file w/o any flag");
-    println!("<OUTPUT>             Out file w/o any flag");
-    println!();
-    println!("TQ:");
-    println!("WORK IN PROGRESS");
-    // println!("-t|--tq 10.0-99.0    Allowed SSIMU2 Range");
-    // println!("-m|--mode p15        Metric mode for TQ: `mean`, or mean below any lowest %");
-    // println!("-c|--qp 4.0-70.0     Allowed CRF/QP range for TQ");
+    println!("xav i.mkv  # Uses all defaults, creates a `scd_i.txt` and output will be named `i_av1.mkv`.");
 }
 
 fn parse_args() -> Args {
